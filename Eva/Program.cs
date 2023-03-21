fc1bfce7757781e52443dbad633ae6863daaba50
Neznak0mec <smershicus@gmail.com>
2023-03-21 09:47:47 +0300

    Create Python server for record audio and recognize. Create C# client for receive result

--- Eva/Program.cs
+++ Eva/Program.cs
@@ -1,17 +1,26 @@
-﻿using NAudio.Wave;
-    using Vosk;
-    
+﻿
+using System.Net.Sockets;
+using System.Text;
+
 namespace Eva;
-
-
 class Program
 {
+        static void Main(string[] args)
+        {
+                // Подключение к серверу
+                TcpClient client = new TcpClient();
+                client.Connect("localhost", 5555);
+                Console.WriteLine("Connected to server");
 
-    static void Main(string[] args)
-    {
-        
-        var model = new Model("model/en");
-        var tools = new Tools(model);
-        tools.Start();
-    }
+                // Получение данных от сервера
+                byte[] data = new byte[2048];
+                NetworkStream stream = client.GetStream();
+
+                while (true)
+                {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        string message = Encoding.UTF8.GetString(data, 0, bytes);
+                        Console.WriteLine($"Received random number: {message}");
+                }
+        }
 }
