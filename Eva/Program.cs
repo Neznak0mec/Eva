1d822c260e2a23938275aca11494327322fdbda2
Neznak0mec <smershicus@gmail.com>
2023-04-06 18:24:13 +0300

    add GUI

--- Eva/Program.cs
+++ Eva/Program.cs
@@ -1,33 +1,27 @@
-﻿using Serilog;
-using Serilog.Sinks.SystemConsole.Themes;
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using System;
+using Eva;
 
-namespace Eva;
+namespace window;
+
 class Program
 {
-    static async Task Main(string[] args)
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static async Task Main(string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console(theme: AnsiConsoleTheme.Sixteen)
-            .WriteTo.File($"logs/ClientLogs{DateTime.Now}.txt")
-            .CreateLogger();
-        Log.Information("Program Started");
-        
-        
-        // Вывод сообщений от сервера
-        MessageHandler handler = new MessageHandler();
-        
-        // Создание и подключение клиента
-        Client client = new Client(handler);
-        client.Connect("localhost", 5555);
-        
-        CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-
-        // Запуск асинхронного получения сообщений
-        Task receiveTask = client.ReceiveMessagesAsync(cancellationTokenSource.Token);
-
-        Console.ReadLine(); // Ожидание ввода для завершения
-        cancellationTokenSource.Cancel();
-
-        await receiveTask; // Ожидание завершения получения сообщений
+        await BackEnd.Start();
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
     }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .LogToTrace()
+            .UseReactiveUI();
 }
